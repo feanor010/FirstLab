b5dba454e1f222ca2d26bc212cdcfb2e7c0efa31
Feanor010 <temathebst@gmail.com>
2023-02-27 02:51:17 +0600

    Некоторые мишени теперь двигаются

--- main.lua
+++ main.lua
@@ -1,26 +1,29 @@
 math.randomseed(os.clock())
+speed = 200
 gameTime = 30
 minSize = 30
-spawnKD = 1
+spawnKD = 0.5
 targets = {}
 maxSize = 50
 windowWidth = love.graphics.getWidth()
 windowHeight = love.graphics.getHeight()
 scores = 0
-bonusTime = spawnKD
+bonusTime = spawnKD / 2
 local Target = {
+  isActive,
   currentWidth = 0,
   currentHeight = 0,
   x,
   y,
   width, 
   height,
-  liveTime = 2
+  liveTime = 2,
+  direction
   
 }
 
 function Target:new ()
-  local obj = {width = math.random(minSize,maxSize),height = math.random(minSize,maxSize), x = math.random(0,windowWidth - 50),y = math.random(0,windowHeight - 50), liveTime = math.random(1,5)}
+  local obj = {width = math.random(minSize,maxSize),height = math.random(minSize,maxSize), x = math.random(0,windowWidth - 50),y = math.random(0,windowHeight - 50), liveTime = math.random(1,3), isActive = math.random(0,1), direction = math.random(1,2)}
   self.__index = self
   setmetatable(obj, self)
   return  obj
@@ -54,7 +57,7 @@ end
 
 
 function StopGame(dt)
-  gameTime = gameTime-dt
+  gameTime = gameTime - dt
   
   if gameTime <= 0 then
     os.exit()
@@ -66,27 +69,41 @@ function DeleteTarget(pos)
   table.remove(targets,#targets)
 end
 
+function Penetration (pos)
+  if targets[pos].isActive == 1 then
+    gameTime = gameTime + bonusTime * 1.5
+    scores = scores + 20
+  else
+    gameTime = gameTime + bonusTime
+    scores = scores + 10
+  end
+  DeleteTarget(pos)
+end
+
 function love.update(dt)
   StopGame(dt)
   spawnKD = spawnKD - dt
   
   if spawnKD <=0 then
     table.insert(targets, Target:new())
-    spawnKD = 1
+    spawnKD = 0.5
   end
   
   for i  = 1, #targets do 
-    if targets[i] ~= nil then
-      targets[i]:IsTimeToDie(dt)
-      
-      if targets[i]:IsTimeToDie(dt) then
-        DeleteTarget(i)
+    if targets[i]~= nil and targets[i].isActive == 1 then
+      if targets[i].direction == 2 then
+        targets[i].x = targets[i].x + dt * speed
+      else 
+        targets[i].x = targets[i].x - dt * speed
       end
-      if targets[i] ~= nil and love.mouse.isDown(1) and targets[i]:IsCursorOnTarget(i) then
-          gameTime = gameTime + bonusTime
-          scores = scores + 10
-          DeleteTarget(i)
-      end
+    end    
+    
+    if targets[i]~= nil and targets[i]:IsTimeToDie(dt) then
+      DeleteTarget(i)
+    end
+    
+    if targets[i] ~= nil and love.mouse.isDown(1) and targets[i]:IsCursorOnTarget(i) then
+      Penetration(i)
     end 
   end
     
